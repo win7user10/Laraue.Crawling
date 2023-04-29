8e3fc8ee9640a1670a5a5fa77ee8569c7fe0f195
Ilya Belyanskiy <win7user20@gmail.com>
2023-04-29 21:42:03 +0300

    make schema public

--- src/Laraue.Crawling.Common/Impl/CompiledHtmlSchema.cs
+++ src/Laraue.Crawling.Common/Impl/CompiledHtmlSchema.cs
@@ -3,12 +3,38 @@ using Laraue.Crawling.Abstractions.Schema.Binding;
 
 namespace Laraue.Crawling.Common.Impl;
 
-internal class CompiledHtmlSchema<TElement, TModel> : ICompiledHtmlSchema<TElement, TModel>
+/// <summary>
+/// The schema which can be used for the parsing.
+/// </summary>
+/// <typeparam name="TElement"></typeparam>
+/// <typeparam name="TModel"></typeparam>
+public class CompiledHtmlSchema<TElement, TModel> : ICompiledHtmlSchema<TElement, TModel>
 {
+    /// <summary>
+    /// Initializes a new instance of <see cref="CompiledHtmlSchema{TElement, TModel}"/>.
+    /// Can be used to create strongly-types schema as a class.
+    /// <example>
+    ///     public class MySchema : ICompiledHtmlSchema&lt;IElement, CianAdvertisementBatch&gt;
+    ///     {
+    ///         public MySchema(Dependency dependency) : base(GetSchema(dependency))
+    ///         {
+    ///         }
+    ///
+    ///         public ICompiledHtmlSchema&lt;IElement, CianAdvertisementBatch&gt; GetSchema(Dependency dependency)
+    ///         {
+    ///             return new PuppeterSharpSchemaBuilder&lt;CianAdvertisementBatch&gt;().Build();
+    ///         }
+    ///     }
+    /// </example>
+    /// </summary>
+    /// <param name="bindingExpression"></param>
     public CompiledHtmlSchema(BindObjectExpression<TElement> bindingExpression)
     {
         BindingExpression = bindingExpression;
     }
     
+    /// <summary>
+    /// Root binding expression.
+    /// </summary>
     public BindObjectExpression<TElement> BindingExpression { get; }
 }
