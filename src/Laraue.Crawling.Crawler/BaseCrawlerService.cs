d456ea0a7b3f6f19c75103711da70d852a1f5043
Ilya Belyanskiy <win7user20@gmail.com>
2023-06-07 22:28:11 +0300

    use backfround service as the crawlng service basis

---
+++ src/Laraue.Crawling.Crawler/BaseCrawlerService.cs
@@ -0,0 +1,86 @@
+﻿using System.Diagnostics;
+using Laraue.Core.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Laraue.Crawling.Crawler;
+
+/// <summary>
+/// Base implementation of the crawler which can store it state.
+/// </summary>
+/// <typeparam name="TModel"></typeparam>
+/// <typeparam name="TLink"></typeparam>
+/// <typeparam name="TState"></typeparam>
+public abstract class BaseCrawlerService<TModel, TLink, TState> : ICrawlerService<TState>
+    where TModel : class
+    where TState : class, new()
+{
+    private readonly ILogger<BaseCrawlerService<TModel, TLink, TState>> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="BaseCrawlerService{TModel,TLink,TState}"/>.
+    /// </summary>
+    /// <param name="logger"></param>
+    protected BaseCrawlerService(ILogger<BaseCrawlerService<TModel, TLink, TState>> logger)
+    {
+        _logger = logger;
+    }
+
+    /// <inheritdoc />
+    public async Task<TimeSpan> ExecuteAsync(JobState<TState> jobState, CancellationToken stoppingToken)
+    {
+        var sessionStopwatch = new Stopwatch();
+        sessionStopwatch.Start();
+        
+        var pageStopwatch = new Stopwatch();
+        pageStopwatch.Start();
+            
+        var link = await GetNextLinkAsync(stoppingToken).ConfigureAwait(false);
+        if (link == null)
+        {
+            _logger.LogInformation("Crawling session finished for {Time}", sessionStopwatch.Elapsed);
+            return GetTimeToWait();
+        }
+            
+        _logger.LogInformation("Page {Page} processing started", link);
+            
+        await ParseLinkAsync(link, stoppingToken).ConfigureAwait(false);
+        await UpdateStateAsync(jobState, stoppingToken).ConfigureAwait(false);
+            
+        _logger.LogInformation(
+            "Page {Page} processing finished for {Time}",
+            link,
+            pageStopwatch.Elapsed);
+        
+        // TODO - Should be opportunity to message job runner that state should be updated and next call be executed immediately
+        return TimeSpan.Zero;
+    }
+
+    /// <summary>
+    /// Return next link should be parsed or null to pause.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    protected abstract Task<TLink?> GetNextLinkAsync(CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Execute something after one link has been parsed. Here 
+    /// </summary>
+    /// <param name="state"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    protected abstract Task UpdateStateAsync(JobState<TState> state, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// The body of parsing.
+    /// </summary>
+    /// <param name="link"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    protected abstract Task<TModel?> ParseLinkAsync(TLink link, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Return how long to wait before the next crawling session.
+    /// </summary>
+    /// <returns></returns>
+    protected abstract TimeSpan GetTimeToWait();
+}
---
+++ src/Laraue.Crawling.Crawler/ICrawlerService.cs
@@ -0,0 +1,13 @@
+﻿using Laraue.Core.Extensions.Hosting;
+using Laraue.Crawling.Abstractions;
+
+namespace Laraue.Crawling.Crawler;
+
+/// <summary>
+/// The <see cref="ICrawler{TState}"/> that contains a state that can be stored somehow.
+/// </summary>
+/// <typeparam name="TState"></typeparam>
+public interface ICrawlerService<TState> : IJob<TState>
+    where TState : class, new()
+{
+}
