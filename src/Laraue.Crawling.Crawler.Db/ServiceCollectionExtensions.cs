f61df5e6ef24e7486e80ae94b75e0d55fb8f4765
Ilya Belyanskiy <win7user20@gmail.com>
2023-06-10 13:55:09 +0300

    add extensions to register crawler with db state

---
+++ src/Laraue.Crawling.Crawler.Db/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Laraue.Core.Extensions.Hosting.EfCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Laraue.Crawling.Crawler.Db;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Add crawler job to the container.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <typeparam name="TCrawlerJob">Crawler job type.</typeparam>
+    /// <typeparam name="TModel">The model receiving from a crawling.</typeparam>
+    /// <typeparam name="TLink">Link type that uses to open next page to parse.</typeparam>
+    /// <typeparam name="TState">Class with the crawler state.</typeparam>
+    /// <returns></returns>
+    public static IServiceCollection AddCrawlingService<TCrawlerJob, TModel, TLink, TState>(
+        this IServiceCollection services)
+        where TCrawlerJob : BaseCrawlerJob<TModel, TLink, TState>
+        where TModel : class
+        where TState : class, new()
+    {
+        return services.AddCrawlingService<TCrawlerJob, TModel, TLink, TState, DbJobRunner<TCrawlerJob, TState>>();
+    }
+}
