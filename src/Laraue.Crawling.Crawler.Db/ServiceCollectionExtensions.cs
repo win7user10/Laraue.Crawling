a4b17034ba3624e9f6fd892b70faaeecd31906b8
Ilya Belyanskiy <win7user20@gmail.com>
2023-06-10 18:04:03 +0300

    pass crawler key while adding in DI

--- src/Laraue.Crawling.Crawler.Db/ServiceCollectionExtensions.cs
+++ src/Laraue.Crawling.Crawler.Db/ServiceCollectionExtensions.cs
@@ -9,17 +9,19 @@ public static class ServiceCollectionExtensions
     /// Add crawler job to the container.
     /// </summary>
     /// <param name="services"></param>
+    /// <param name="jobKey"></param>
     /// <typeparam name="TCrawlerJob">Crawler job type.</typeparam>
     /// <typeparam name="TModel">The model receiving from a crawling.</typeparam>
     /// <typeparam name="TLink">Link type that uses to open next page to parse.</typeparam>
     /// <typeparam name="TState">Class with the crawler state.</typeparam>
     /// <returns></returns>
     public static IServiceCollection AddCrawlingService<TCrawlerJob, TModel, TLink, TState>(
-        this IServiceCollection services)
+        this IServiceCollection services,
+        string jobKey)
         where TCrawlerJob : BaseCrawlerJob<TModel, TLink, TState>
         where TModel : class
         where TState : class, new()
     {
-        return services.AddCrawlingService<TCrawlerJob, TModel, TLink, TState, DbJobRunner<TCrawlerJob, TState>>();
+        return services.AddCrawlingService<TCrawlerJob, TModel, TLink, TState, DbJobRunner<TCrawlerJob, TState>>(jobKey);
     }
 }
--- src/Laraue.Crawling.Crawler/ServiceCollectionExtensions.cs
+++ src/Laraue.Crawling.Crawler/ServiceCollectionExtensions.cs
@@ -9,6 +9,7 @@ public static class ServiceCollectionExtensions
     /// Add crawler job to the container.
     /// </summary>
     /// <param name="services"></param>
+    /// <param name="jobKey"></param>
     /// <typeparam name="TCrawlerJob">Crawler job type.</typeparam>
     /// <typeparam name="TModel">The model receiving from a crawling.</typeparam>
     /// <typeparam name="TLink">Link type that uses to open next page to parse.</typeparam>
@@ -16,12 +17,13 @@ public static class ServiceCollectionExtensions
     /// <typeparam name="TJobRunner">Class that will run the job and manage it state.</typeparam>
     /// <returns></returns>
     public static IServiceCollection AddCrawlingService<TCrawlerJob, TModel, TLink, TState, TJobRunner>(
-        this IServiceCollection services)
+        this IServiceCollection services,
+        string jobKey)
         where TCrawlerJob : BaseCrawlerJob<TModel, TLink, TState>
         where TModel : class
         where TState : class, new()
         where TJobRunner : JobRunner<TCrawlerJob, TState>
     {
-        return services.AddBackgroundJob<TCrawlerJob, TState, TJobRunner>();
+        return services.AddBackgroundJob<TCrawlerJob, TState, TJobRunner>(jobKey);
     }
 }
